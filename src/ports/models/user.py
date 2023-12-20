3c26121f0185fa32a9e9e082f115027c24947965
kosmp <kosmin.2003@mail.ru>
2023-12-20 18:06:17 +0300

    fixed user pydantic model

--- src/ports/models/user.py
+++ src/ports/models/user.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, constr
+from pydantic import BaseModel, constr, UUID5
 from ports.enums import Role
 from typing import Optional
 
@@ -11,13 +11,21 @@ class ValidatedPassword(BaseModel):
     password: constr(min_length=8, max_length=15)
 
 
+class ValidatedName(BaseModel):
+    name: constr(min_length=1, max_length=15)
+
+
+class ValidatedPhoneNumber(BaseModel):
+    phone_number: constr(regex=r"^\+?[1-9]\d{1,14}$")
+
+
 class UserUpdateData(BaseModel):
-    email: Optional[ValidatedEmail]
-    name: Optional[constr(min_length=1, max_length=15)]
-    surname: Optional[constr(min_length=1, max_length=15)]
-    password: Optional[ValidatedPassword]
-    phone_number: Optional[constr(regex=r"^\+?[1-9]\d{1,14}$")]
+    email: Optional[ValidatedEmail.email]
+    name: Optional[ValidatedName.name]
+    surname: Optional[ValidatedName.name]
+    password: Optional[ValidatedPassword.password]
+    phone_number: Optional[ValidatedPhoneNumber.phone_number]
     role: Optional[Role]
-    group_id: Optional[int]
+    group_id: Optional[UUID5]
     image: Optional[str]
     is_blocked: Optional[bool]
