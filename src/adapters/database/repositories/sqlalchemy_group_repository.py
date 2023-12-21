1645c2179cd469853ec8843b9292e58684d8ae20
kosmp <kosmin.2003@mail.ru>
2023-12-21 20:19:04 +0300

    changed session to async session in repositories

--- src/adapters/database/repositories/sqlalchemy_group_repository.py
+++ src/adapters/database/repositories/sqlalchemy_group_repository.py
@@ -3,14 +3,14 @@ from sqlalchemy import select, delete
 from adapters.database.models.groups import Group
 from app.exceptions import DatabaseConnectionException
 from ports.repositories.group_repository import GroupRepository
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from ports.schemas.group import CreateGroupModel
 from pydantic import UUID5
 from typing import Union
 
 
 class SQLAlchemyGroupRepository(GroupRepository):
-    def __init__(self, db_session: Session):
+    def __init__(self, db_session: AsyncSession):
         self.db_session = db_session
 
     async def create_group(self, group_name: CreateGroupModel.group_name) -> Group:
@@ -28,7 +28,7 @@ class SQLAlchemyGroupRepository(GroupRepository):
     async def get_group(self, group_id: UUID5) -> Union[Group, None]:
         try:
             query = select(Group).where(Group.id == group_id)
-            res = await self.db_session.execute(query).fetchone()
+            res = (await self.db_session.execute(query)).fetchone()
 
             if res is not None:
                 return res[0]
@@ -38,7 +38,7 @@ class SQLAlchemyGroupRepository(GroupRepository):
     async def delete_group(self, group_id: UUID5) -> bool:
         try:
             query = delete(Group).where(Group.id == group_id)
-            res = await self.db_session.execute(query).fetchone()
+            res = (await self.db_session.execute(query)).fetchone()
 
             if res is None:
                 return False
--- src/adapters/database/repositories/sqlalchemy_user_repository.py
+++ src/adapters/database/repositories/sqlalchemy_user_repository.py
@@ -3,14 +3,14 @@ from sqlalchemy import select, update
 from datetime import datetime, timezone
 from app.exceptions import DatabaseConnectionException
 from ports.repositories.user_repository import UserRepository
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from ports.schemas.user import UserUpdateModel, UserCreateModel, UserResponseModel
 from adapters.database.models.users import User
 from typing import Union
 
 
 class SQLAlchemyUserRepository(UserRepository):
-    def __init__(self, db_session: Session):
+    def __init__(self, db_session: AsyncSession):
         self.db_session = db_session
 
     async def create_user(self, user_data: UserCreateModel) -> UserResponseModel:
@@ -33,7 +33,7 @@ class SQLAlchemyUserRepository(UserRepository):
     async def get_user(self, user_id: UUID5) -> Union[UserResponseModel, None]:
         try:
             query = select(User).where(User.id == user_id)
-            res = await self.db_session.execute(query).fetchone()
+            res = (await self.db_session.execute(query)).fetchone()
 
             if res is not None:
                 return UserResponseModel(**res[0].dict())
@@ -50,7 +50,7 @@ class SQLAlchemyUserRepository(UserRepository):
                 .values(**user_data, modified_at=datetime.now(timezone.utc))
                 .returning(User.id)
             )
-            res = await self.db_session.execute(query).fetchone()
+            res = (await self.db_session.execute(query)).fetchone()
 
             if res is not None:
                 return UserResponseModel(**res[0].dict())
@@ -66,7 +66,7 @@ class SQLAlchemyUserRepository(UserRepository):
                 .values(is_blocked=True)
                 .returning(User.id)
             )
-            res = await self.db_session.execute(query).fetchone()
+            res = (await self.db_session.execute(query)).fetchone()
 
             if res is not None:
                 return UserResponseModel(**res[0].dict())
