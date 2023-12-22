f1e55373709fe84517974b5882965f14c8076d81
kosmp <kosmin.2003@mail.ru>
2023-12-22 20:05:23 +0300

    added credentials schema and fixed types in schemas

--- src/ports/schemas/user.py
+++ src/ports/schemas/user.py
@@ -1,13 +1,26 @@
-from pydantic import BaseModel, constr, UUID5
+from pydantic import BaseModel, constr, UUID5, EmailStr, field_validator
 from src.ports.enums import Role
 from typing import Optional
+
+
+class CredentialsModel(BaseModel):
+    email: EmailStr
+    password: constr(min_length=8)
+
+    @field_validator("password")
+    def validate_password(cls, value):
+        if not any(c.isalpha() for c in value):
+            raise ValueError("Password must contain at least one letter")
+        if not any(c.isdigit() for c in value):
+            raise ValueError("Password must contain at least one digit")
+        return value
 
 
 class UserBase(BaseModel):
     name: constr(min_length=1, max_length=15)
     surname: constr(min_length=1, max_length=15)
-    email: constr(regex=r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
-    phone_number: constr(regex=r"^\+?[1-9]\d{1,14}$")
+    email: EmailStr
+    phone_number: constr(pattern=r"^\+?[1-9]\d{1,14}$")
     is_blocked: bool
     image: str
 
@@ -18,17 +31,17 @@ class UserCreateModel(UserBase):
     role: Role = Role.USER
 
 
-class UserUpdateModel(UserBase):
-    email: Optional[UserBase.email]
-    name: Optional[UserBase.name]
-    surname: Optional[UserBase.surname]
-    phone_number: Optional[UserBase.phone_number]
-    image: Optional[UserBase.image]
-    is_blocked: Optional[UserBase.is_blocked]
-    role: Optional[Role]
-    group_id: Optional[UUID5]
-
-
 class UserResponseModel(UserBase):
     group_id: UUID5
     role: Role
+
+
+class UserUpdateModel(BaseModel):
+    email: Optional[EmailStr]
+    name: Optional[constr(min_length=1, max_length=15)]
+    surname: Optional[constr(min_length=1, max_length=15)]
+    phone_number: Optional[constr(pattern=r"^\+?[1-9]\d{1,14}$")]
+    image: Optional[str]
+    is_blocked: Optional[bool]
+    role: Optional[Role]
+    group_id: Optional[UUID5]
