25fed1880f2e1549d96dbbeb973a6c4af129cda4
kosmp <kosmin.2003@mail.ru>
2023-12-24 15:09:40 +0300

    changed user schemas

--- src/ports/schemas/user.py
+++ src/ports/schemas/user.py
@@ -1,6 +1,35 @@
-from pydantic import BaseModel, constr, UUID5, EmailStr, field_validator
+from pydantic import BaseModel, constr, UUID5, EmailStr, field_validator, Field
 from src.ports.enums import Role
 from typing import Optional
+
+
+class UserBase(BaseModel):
+    email: EmailStr
+    name: Optional[constr(min_length=1, max_length=15)]
+    surname: Optional[constr(min_length=1, max_length=15)]
+    phone_number: Optional[constr(pattern=r"^\+?[1-9]\d{1,14}$")]
+    is_blocked: Optional[bool]
+    image: Optional[str]
+
+
+class UserCreateModel(UserBase):
+    password: str
+    group_id: UUID5
+    role: Role = Role.USER
+
+
+class SignUpModel(UserBase):
+    password: constr(min_length=8)
+    group_id: UUID5
+    role: Role = Role.USER
+
+    @field_validator("password")
+    def validate_password(cls, value):
+        if not any(c.isalpha() for c in value):
+            raise ValueError("Password must contain at least one letter")
+        if not any(c.isdigit() for c in value):
+            raise ValueError("Password must contain at least one digit")
+        return value
 
 
 class CredentialsModel(BaseModel):
@@ -16,22 +45,8 @@ class CredentialsModel(BaseModel):
         return value
 
 
-class UserBase(BaseModel):
-    name: constr(min_length=1, max_length=15)
-    surname: constr(min_length=1, max_length=15)
-    email: EmailStr
-    phone_number: constr(pattern=r"^\+?[1-9]\d{1,14}$")
-    is_blocked: bool
-    image: str
-
-
-class UserCreateModel(UserBase):
-    password: str
-    group_id: UUID5
-    role: Role = Role.USER
-
-
 class UserResponseModel(UserBase):
+    id: UUID5
     group_id: UUID5
     role: Role
 
