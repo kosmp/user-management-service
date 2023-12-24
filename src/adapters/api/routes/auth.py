2450765daf0a2e638baff3611d8f0b1e35d17e5c
kosmp <kosmin.2003@mail.ru>
2023-12-24 16:30:21 +0300

    handler signatures have been changed. Added logic for get_users and get_user handlers

--- src/adapters/api/routes/auth.py
+++ src/adapters/api/routes/auth.py
@@ -1,23 +1,25 @@
 from fastapi import APIRouter
+from pydantic import EmailStr
+from src.ports.schemas.user import SignUpModel, UserResponseModel, CredentialsModel
 
 router = APIRouter()
 
 
-@router.post("/auth/signup")
-async def signup(email: str, password: str):
+@router.post("/auth/signup", response_model=UserResponseModel)
+async def signup(user_data: SignUpModel):
     pass
 
 
 @router.post("/auth/login")
-async def login(email: str):
+async def login(email: EmailStr):
     pass
 
 
 @router.post("/auth/refresh-token")
-async def refresh_token(token: str):
+async def refresh_token():
     pass
 
 
 @router.post("/auth/refresh-password")
-async def refresh_password(email: str, password: str):
+async def refresh_password(credentials: CredentialsModel):
     pass
--- src/adapters/api/routes/user.py
+++ src/adapters/api/routes/user.py
@@ -1,39 +1,66 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from typing import List
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import UUID
+
+from src.ports.schemas.user import UserResponseModel, UserUpdateModel
+from src.adapters.database.database_settings import get_async_session
+from src.adapters.database.repositories.sqlalchemy_user_repository import (
+    SQLAlchemyUserRepository,
+)
 
 router = APIRouter()
 
 
-@router.get("/users")
+@router.get("/users", response_model=List[UserResponseModel])
 async def get_users(
-    page: int,
+    page: int = 1,
     limit: int = 30,
     filter_by_name: str = None,
     sort_by: str = None,
     order_by: str = "asc",
+    db_session: AsyncSession = Depends(get_async_session),
+):
+    db_users = await SQLAlchemyUserRepository(db_session).get_users(
+        page, limit, filter_by_name, sort_by, order_by
+    )
+    if len(db_users) == 0:
+        raise HTTPException(status_code=404, detail="Users not found")
+    return db_users
+
+
+@router.get("/user/me", response_model=UserResponseModel)
+async def get_me(db_session: AsyncSession = Depends(get_async_session)):
+    pass
+
+
+@router.patch("/user/me", response_model=UserResponseModel)
+async def update_me(
+    update_data: UserUpdateModel, db_session: AsyncSession = Depends(get_async_session)
 ):
     pass
 
 
-@router.get("/user/me")
-async def get_me():
+@router.delete("/user/me", response_model=UserResponseModel)
+async def delete_me(db_session: AsyncSession = Depends(get_async_session)):
     pass
 
 
-@router.patch("/user/me")
-async def update_me():
-    pass
+@router.get("/user/{user_id}", response_model=UserResponseModel)
+async def get_user(
+    user_id: UUID, db_session: AsyncSession = Depends(get_async_session)
+):
+    db_user = await SQLAlchemyUserRepository(db_session).get_user(user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
 
 
-@router.delete("/user/me")
-async def delete_me():
-    pass
-
-
-@router.get("/user/{user_id}")
-async def get_user(user_id: int):
-    pass
-
-
-@router.patch("/user/{user_id}")
-async def update_user(user_id: int):
+@router.patch("/user/{user_id}", response_model=UserResponseModel)
+async def update_user(
+    user_id: int,
+    update_data: UserUpdateModel,
+    db_session: AsyncSession = Depends(get_async_session),
+):
     pass
