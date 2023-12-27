d0913717286e50016212d87397fced834274ad13
kosmp <kosmin.2003@mail.ru>
2023-12-27 14:18:36 +0300

    added GroupResponseModel scheme

--- src/ports/schemas/group.py
+++ src/ports/schemas/group.py
@@ -1,5 +1,12 @@
-from pydantic import BaseModel, constr
+from pydantic import BaseModel, constr, UUID5
+from datetime import datetime
 
 
 class CreateGroupModel(BaseModel):
     group_name: constr(min_length=1, max_length=15)
+
+
+class GroupResponseModel(BaseModel):
+    id: UUID5
+    name: constr(min_length=1, max_length=15)
+    created_at: datetime
