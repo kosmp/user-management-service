1e405d2498b330be238b2255f8d0bde46c3c8b77
kosmp <kosmin.2003@mail.ru>
2023-12-27 15:46:20 +0300

    added group actions

---
+++ src/core/actions/group.py
@@ -0,0 +1,35 @@
+from fastapi import HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.ports.schemas.group import GroupResponseModel, CreateGroupModel
+from src.adapters.database.repositories.sqlalchemy_group_repository import (
+    SQLAlchemyGroupRepository,
+)
+
+
+async def create_db_group(
+    group_name: CreateGroupModel.group_name, db_session: AsyncSession
+) -> GroupResponseModel:
+    db_group = await SQLAlchemyGroupRepository(db_session).create_group(group_name)
+
+    if db_group is None:
+        raise HTTPException(status_code=404, detail="Group not found")
+    return db_group
+
+
+async def get_db_group(group_id: str, db_session: AsyncSession) -> GroupResponseModel:
+    db_group = await SQLAlchemyGroupRepository(db_session).get_group(group_id)
+
+    if db_group is None:
+        raise HTTPException(status_code=404, detail="Group not found")
+    return db_group
+
+
+async def delete_db_group(group_id: str, db_session: AsyncSession) -> str:
+    deleted_group_id = await SQLAlchemyGroupRepository(db_session).delete_group(
+        group_id
+    )
+
+    if deleted_group_id is None:
+        raise HTTPException(status_code=422, detail="Group can't be deleted")
+
+    return deleted_group_id
