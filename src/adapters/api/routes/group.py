c4fc74cc1340bb8a7c230ad518ad338292e65d84
kosmp <kosmin.2003@mail.ru>
2023-12-27 15:52:22 +0300

    fixed group route urls

--- src/adapters/api/routes/group.py
+++ src/adapters/api/routes/group.py
@@ -7,7 +7,7 @@ from src.core.actions.group import get_db_group, delete_db_group, create_db_grou
 router = APIRouter()
 
 
-@router.post("/groups", response_model=GroupResponseModel)
+@router.post("/group", response_model=GroupResponseModel)
 async def create_group(
     group_name: CreateGroupModel.group_name,
     db_session: AsyncSession = Depends(get_async_session),
@@ -15,14 +15,14 @@ async def create_group(
     return await create_db_group(group_name, db_session)
 
 
-@router.get("/groups", response_model=GroupResponseModel)
+@router.get("/group/{group_id}", response_model=GroupResponseModel)
 async def get_group(
     group_id: str, db_session: AsyncSession = Depends(get_async_session)
 ):
     return await get_db_group(group_id, db_session)
 
 
-@router.delete("/groups", response_model=str)
+@router.delete("/group/{group_id}", response_model=str)
 async def delete_group(
     group_id: str, db_session: AsyncSession = Depends(get_async_session)
 ):
