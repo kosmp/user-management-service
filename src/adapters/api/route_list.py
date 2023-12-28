60dd55c0f0f742e094a9c1f28a219b28dd5d616d
kosmp <kosmin.2003@mail.ru>
2023-12-28 14:26:29 +0300

    added versions to router

--- src/adapters/api/route_list.py
+++ src/adapters/api/route_list.py
@@ -2,6 +2,6 @@ from src.adapters.api.routes import auth, healthcheck, user, group
 from src.app import app
 
 app.include_router(healthcheck.router, tags=["Healthcheck"])
-app.include_router(user.router, tags=["User"])
-app.include_router(auth.router, tags=["Auth"])
-app.include_router(group.router, tags=["Group"])
+app.include_router(user.router, prefix="/v1", tags=["User v1"])
+app.include_router(auth.router, prefix="/v1", tags=["Auth v1"])
+app.include_router(group.router, prefix="/v1", tags=["Group v1"])
