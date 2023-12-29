57c120dbc10f73354f76d0712cceb0ed7fad55a5
kosmp <kosmin.2003@mail.ru>
2023-12-29 16:59:02 +0300

    changes with router versions

--- src/adapters/api/route_list.py
+++ src/adapters/api/route_list.py
@@ -1,7 +1,14 @@
+from fastapi import APIRouter
+
 from src.adapters.api.routes import auth, healthcheck, user, group
 from src.app import app
 
+main_router = APIRouter()
+
 app.include_router(healthcheck.router, tags=["Healthcheck"])
-app.include_router(user.router, prefix="/v1", tags=["User v1"])
-app.include_router(auth.router, prefix="/v1", tags=["Auth v1"])
-app.include_router(group.router, prefix="/v1", tags=["Group v1"])
+
+main_router.include_router(user.router, tags=["User v1"])
+main_router.include_router(auth.router, tags=["Auth v1"])
+main_router.include_router(group.router, tags=["Group v1"])
+
+app.include_router(main_router, prefix="v1")
