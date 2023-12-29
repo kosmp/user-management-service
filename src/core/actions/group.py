15fd0fa40933331c35345666acd30bad65461499
kosmp <kosmin.2003@mail.ru>
2023-12-29 14:41:16 +0300

    duplicate exceptions removed from actions

--- src/core/actions/group.py
+++ src/core/actions/group.py
@@ -10,27 +10,12 @@ from src.adapters.database.repositories.sqlalchemy_group_repository import (
 async def create_db_group(
     group_name: CreateGroupModel.group_name, db_session: AsyncSession
 ) -> GroupResponseModel:
-    db_group = await SQLAlchemyGroupRepository(db_session).create_group(group_name)
-
-    if db_group is None:
-        raise HTTPException(status_code=500, detail="Group wasn't created")
-    return db_group
+    return await SQLAlchemyGroupRepository(db_session).create_group(group_name)
 
 
 async def get_db_group(group_id: UUID5, db_session: AsyncSession) -> GroupResponseModel:
-    db_group = await SQLAlchemyGroupRepository(db_session).get_group(group_id)
-
-    if db_group is None:
-        raise HTTPException(status_code=404, detail="Group not found")
-    return db_group
+    return await SQLAlchemyGroupRepository(db_session).get_group(group_id)
 
 
 async def delete_db_group(group_id: UUID5, db_session: AsyncSession) -> UUID5:
-    deleted_group_id = await SQLAlchemyGroupRepository(db_session).delete_group(
-        group_id
-    )
-
-    if deleted_group_id is None:
-        raise HTTPException(status_code=422, detail="Group can't be deleted")
-
-    return deleted_group_id
+    return await SQLAlchemyGroupRepository(db_session).delete_group(group_id)
--- src/core/actions/user.py
+++ src/core/actions/user.py
@@ -11,39 +11,18 @@ from src.adapters.database.repositories.sqlalchemy_user_repository import (
 async def get_updated_db_user(
     user_id: UUID5, update_data: UserUpdateModel, db_session: AsyncSession
 ) -> UserResponseModel:
-    result_user = await SQLAlchemyUserRepository(db_session).update_user(
+    return await SQLAlchemyUserRepository(db_session).update_user(
         user_id, **update_data.model_dump()
     )
 
-    if result_user is None:
-        raise HTTPException(
-            status_code=404, detail="User not found. So can't be updated"
-        )
-    return result_user
-
 
 async def get_db_user(user_id: UUID5, db_session: AsyncSession) -> UserResponseModel:
-    db_user = await SQLAlchemyUserRepository(db_session).get_user(user_id)
-
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
+    return await SQLAlchemyUserRepository(db_session).get_user(user_id)
 
 
 async def block_db_user(user_id: UUID5, db_session: AsyncSession) -> UserResponseModel:
-    result_user = await SQLAlchemyUserRepository(db_session).block_user(user_id)
-
-    if result_user is None:
-        raise HTTPException(
-            status_code=404, detail="User not found. So can't be blocked"
-        )
-    return result_user
+    return await SQLAlchemyUserRepository(db_session).block_user(user_id)
 
 
 async def delete_db_user(user_id: UUID5, db_session: AsyncSession) -> UUID5:
-    deleted_user_id = await SQLAlchemyUserRepository(db_session).delete_user(user_id)
-
-    if deleted_user_id is None:
-        raise HTTPException(status_code=404, detail="User not found for deletion")
-
-    return deleted_user_id
+    return await SQLAlchemyUserRepository(db_session).delete_user(user_id)
