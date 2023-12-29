7c5044ad37f36b135d9e94120e0fac372a982720
kosmp <kosmin.2003@mail.ru>
2023-12-29 13:33:58 +0300

    changed credentials exception to class

--- src/core/services/token.py
+++ src/core/services/token.py
@@ -1,23 +1,18 @@
 from uuid import UUID
 
 from jose import JWTError, jwt
-from fastapi import HTTPException, status
 from src.ports.schemas.user import TokenData
+from src.core.exceptions import CredentialsException
 
 
 def get_token_data(token) -> TokenData:
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         user_id: UUID = payload.get("sub")
         if user_id is None:
-            raise credentials_exception
+            raise CredentialsException
 
         token_data = TokenData(user_id=user_id)
         return token_data
     except JWTError:
-        raise credentials_exception
+        raise CredentialsException
