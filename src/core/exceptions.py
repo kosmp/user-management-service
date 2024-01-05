a13d9bae5465e1978d781e690db88ef30fe802b4
kosmp <kosmin.2003@mail.ru>
2024-01-05 18:40:09 +0300

    added headers to CredentialsException

--- src/core/exceptions.py
+++ src/core/exceptions.py
@@ -6,6 +6,7 @@ class CredentialsException(HTTPException):
         super().__init__(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
         )
 
 
