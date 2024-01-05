52aeb3d9ddcf2eac8cb545560c077d14d1d94dd8
kosmp <kosmin.2003@mail.ru>
2024-01-05 17:16:45 +0300

    changed id name in get_db_user_by_id

--- src/core/actions/user.py
+++ src/core/actions/user.py
@@ -18,7 +18,7 @@ async def get_updated_db_user(
 async def get_db_user_by_id(
     user_id: UUID5, db_session: AsyncSession
 ) -> UserResponseModel:
-    return await SQLAlchemyUserRepository(db_session).get_user(user_id=user_id)
+    return await SQLAlchemyUserRepository(db_session).get_user(id=user_id)
 
 
 async def get_db_user_by_email(
