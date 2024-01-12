c463a360d9bcba5def99c6f3e3cc3c92280f9a9b
kosmp <kosmin.2003@mail.ru>
2024-01-12 13:09:21 +0300

    added rabbitmq to docker

--- src/core/config.py
+++ src/core/config.py
@@ -15,6 +15,8 @@ class PydanticSettings(BaseSettings):
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 30
     refresh_token_expire_minutes: int = 60 * 24 * 7
+    rabbitmq_default_user: str = None
+    rabbitmq_default_pass: str = None
 
     @property
     def get_db_creds(self):
