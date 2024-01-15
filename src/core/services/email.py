fa5cae01a3703b0f9cf96cfcedec1dcfd161e097
kosmp <kosmin.2003@mail.ru>
2024-01-15 12:56:06 +0300

    added send_verify_email logic to EmailService

--- src/core/services/email.py
+++ src/core/services/email.py
@@ -9,9 +9,9 @@ conf = ConnectionConfig(
     MAIL_PORT=settings.mail_port,
     MAIL_SERVER=settings.mail_server,
     MAIL_FROM_NAME=settings.mail_from_name,
-    MAIL_TLS=True,
-    MAIL_SSL=False,
-    USE_CREDENTIALS=True,
+    MAIL_TLS=settings.mail_tls,
+    MAIL_SSL=settings.mail_ssl,
+    USE_CREDENTIALS=settings.mail_use_credentials,
     TEMPLATES_FOLDER="src/templates",
 )
 
@@ -20,8 +20,16 @@ class EmailService:
     def __init__(self, email_to: EmailStr) -> None:
         self.email_to = email_to
 
-    def send_verify_email(self, subject: str, body: str):
-        pass
+    async def send_verify_email(self, subject: str, body: dict):
+        message = MessageSchema(
+            subject=subject,
+            recipients=[str(self.email_to)],
+            template_body=body,
+            subtype="html",
+        )
+
+        fm = FastMail(conf)
+        await fm.send_message(message, template_name="account-verification.html")
 
     async def send_reset_password_email(self, subject: str, body: dict):
         message = MessageSchema(
