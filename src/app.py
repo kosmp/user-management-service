d32a15d749bfa98fc3992bcfe646b94c20d2f9c5
kosmp <kosmin.2003@mail.ru>
2024-01-16 10:36:38 +0300

    added logging middleware

--- src/app.py
+++ src/app.py
@@ -1,3 +1,6 @@
 from fastapi import FastAPI
+from py_fastapi_logging.middlewares.logging import LoggingMiddleware
 
 app = FastAPI()
+
+app.add_middleware(LoggingMiddleware, app_name="user-management-service")
