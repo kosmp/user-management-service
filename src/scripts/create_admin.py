343cbde09586279c8cd48d85875945d7f970a84c
kosmp <kosmin.2003@mail.ru>
2024-01-17 14:21:52 +0300

    fixed create_admin script. Added pass hashing

--- src/scripts/create_admin.py
+++ src/scripts/create_admin.py
@@ -4,6 +4,7 @@ from src.adapters.database.database_settings import async_session
 from src.adapters.database.models.groups import Group
 from src.adapters.database.models.users import User
 from src.core import settings
+from src.core.services.hasher import PasswordHasher
 
 
 async def create_admin_user():
@@ -20,7 +21,7 @@ async def create_admin_user():
             username=settings.admin_username,
             phone_number=settings.admin_phone_number,
             email=settings.admin_email,
-            password=settings.admin_password,
+            password=PasswordHasher.get_password_hash(settings.admin_password),
             group_id=new_group.id,
         )
 
