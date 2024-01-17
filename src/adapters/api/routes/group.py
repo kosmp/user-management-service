f8603372a03649a8ea3a08eb259643278d6f62a8
kosmp <kosmin.2003@mail.ru>
2024-01-17 17:20:53 +0300

    added permissions for groups

--- src/adapters/api/routes/group.py
+++ src/adapters/api/routes/group.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, Security
 from fastapi.security import HTTPAuthorizationCredentials
 from pydantic import UUID4
 
+from src.core.services.user import check_current_user_for_admin
 from src.core import security
 from src.ports.schemas.group import GroupResponseModel, CreateGroupModel, GroupNameType
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -11,28 +12,36 @@ from src.core.actions.group import get_db_group, delete_db_group, create_db_grou
 router = APIRouter()
 
 
-@router.post("/group", response_model=GroupResponseModel)
+@router.post(
+    "/group",
+    response_model=GroupResponseModel,
+    dependencies=[Depends(check_current_user_for_admin)],
+)
 async def create_group(
     group_name: GroupNameType,
     db_session: AsyncSession = Depends(get_async_session),
-    token: HTTPAuthorizationCredentials = Security(security),
 ):
     return await create_db_group(group_name, db_session)
 
 
-@router.get("/group/{group_id}", response_model=GroupResponseModel)
+@router.get(
+    "/group/{group_id}",
+    response_model=GroupResponseModel,
+    dependencies=[Depends(check_current_user_for_admin)],
+)
 async def get_group(
-    group_id: UUID4,
-    db_session: AsyncSession = Depends(get_async_session),
-    token: HTTPAuthorizationCredentials = Security(security),
+    group_id: UUID4, db_session: AsyncSession = Depends(get_async_session)
 ):
     return await get_db_group(group_id, db_session)
 
 
-@router.delete("/group/{group_id}", response_model=UUID4)
+@router.delete(
+    "/group/{group_id}",
+    response_model=UUID4,
+    dependencies=[Depends(check_current_user_for_admin)],
+)
 async def delete_group(
     group_id: UUID4,
     db_session: AsyncSession = Depends(get_async_session),
-    token: HTTPAuthorizationCredentials = Security(security),
 ):
     return await delete_db_group(group_id, db_session)
