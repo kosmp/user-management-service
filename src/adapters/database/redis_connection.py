3a187b9dbd64dbd92c3128a440d724b5f03d141e
kosmp <kosmin.2003@mail.ru>
2024-01-18 11:58:05 +0300

    added volumes for redis

--- src/adapters/database/redis_connection.py
+++ src/adapters/database/redis_connection.py
@@ -1,6 +1,4 @@
 import redis
 from src.core import settings
 
-redis_client = redis.Redis(
-    host=settings.redis_host, port=settings.redis_port, password=settings.redis_password
-)
+redis_client = redis.Redis(host=settings.redis_host, port=settings.redis_port)
