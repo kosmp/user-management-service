b4823d8516d38b16cdffea71b6b6d4df19a108d3
kosmp <kosmin.2003@mail.ru>
2024-01-18 19:14:09 +0300

    removed ses from localstack in docker and script

--- src/scripts/localstack_script.sh
+++ src/scripts/localstack_script.sh
@@ -5,5 +5,3 @@ awslocal s3api \
 create-bucket --bucket mybucket \
 --create-bucket-configuration LocationConstraint=eu-central-1 \
 --region eu-central-1
-
-awslocal ses verify-email-identity --email user-management-service@gmail.com
