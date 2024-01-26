13e19b154cd5a177b90145cb0d2d00a546e8267d
kosmp <kosmin.2003@mail.ru>
2024-01-26 19:25:32 +0300

    added requirements-dev.txt for local tests

--- src/scripts/entrypoint_tests.sh
+++ src/scripts/entrypoint_tests.sh
@@ -1,10 +1,6 @@
 #! /bin/bash
 alembic upgrade head
-python -m src.scripts.create_admin
 
-python -m pip install pytest==7.4.4
-python -m pip install pytest_asyncio==0.23.3
-python -m pip install pytest-mock==3.12.0
-python -m pip install httpx==0.26.0
+python -m pip install -r requirements-dev.txt
 
 python -m pytest --disable-warnings
